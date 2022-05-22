e40935a33cfccbf26080bda39b041c3bea8e2a34
sajjadcodes <sakraan01@gmail.com>
2022-05-22 20:52:42 +0500

    add: retrive wallet's credentials

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const {
     LAMPORTS_PER_SOL
 } = require("@solana/web3.js");
 
+// creating wallet 
 const wallet = new Keypair();
 
-console.log(wallet);
+// console.log(wallet);
+
+// retrive wallet's credentials
+
+const publicKey = wallet._keypair.publicKey;
+const secretKey = wallet._keypair.secretKey;
+
+console.log(publicKey);
+console.log(secretKey);
