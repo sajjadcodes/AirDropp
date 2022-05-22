f1483b97474c4c87e06ddd8baf41ba5698188308
sajjadcodes <sakraan01@gmail.com>
2022-05-22 20:46:28 +0500

    Create: create wallet

---
+++ app.js
@@ -0,0 +1,11 @@
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL
+} = require("@solana/web3.js");
+
+const wallet = new Keypair();
+
+console.log(wallet);
